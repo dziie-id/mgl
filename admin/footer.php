0cf6969dd9d9088ced4c3ca6e0857f85c7e73e16
dziie-id <alfindp@gmail.com>
2026-02-12 04:17:17 +0700

    slim

--- admin/footer.php
+++ admin/footer.php
@@ -35,8 +35,10 @@
 
     Fancybox.bind("[data-fancybox]", {});
 </script>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
     $(document).ready(function() {
         $('.summernote').summernote({
